f4de93558186a6ef2097219afb36c10e2d33bc60
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-01-14 11:21:59 +0100

    Intro code
    Code and scene related to intro.
    Also added 2 test backgrounds for it.
    
    Now to add Lord Jermo.

---
+++ Reapiesta/Assets/Scripts/Menu/Cutscene.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cutscene : MonoBehaviour
+{
+
+    [SerializeField]
+    Text txt;
+    [SerializeField]
+    Image background;
+    [SerializeField]
+    [TextArea(0, 10)]
+    string[] dialogue;
+    int curDia = 0;
+    [SerializeField]
+    Sprite[] backgrounds;
+    Color textColor;
+    [SerializeField]
+    float transitionTime = 3;
+    [SerializeField]
+    Text nextDiaIndicator;
+    AudioSource source;
+    [SerializeField]
+    AudioClip[] spokenDialogue;
+
+    void Start()
+    {
+        textColor = txt.color;
+        source = GetComponent<AudioSource>();
+        source.clip = spokenDialogue[curDia];
+        source.Play();
+    }
+
+    void LateUpdate()
+    {
+        NextDia();
+        SetText();
+        SetBackground();
+    }
+
+    void SpokenDialogue()
+    {
+
+        source.Stop();
+        if (spokenDialogue[curDia] != null)
+        {
+            source.clip = spokenDialogue[curDia];
+            source.Play();
+
+        }
+
+
+    }
+
+    void NextDia()
+    {
+        if (Input.GetButtonDown("Attack") == true && background.color == Color.white && nextDiaIndicator.color == textColor)
+        {
+            if (curDia < dialogue.Length - 1) {
+                curDia++;
+                SpokenDialogue();
+            } else
+            {
+                Debug.Log("End");
+            }
+        }
+    }
+
+    void SetBackground()
+    {
+        if (background.color != Color.white)
+        {
+            background.color = Color.Lerp(background.color, Color.white, transitionTime);
+        }
+        if (backgrounds[curDia] != null)
+        {
+            background.sprite = backgrounds[curDia];
+            if (Input.GetButtonDown("Attack") == true && background.color == Color.white && nextDiaIndicator.color == textColor)
+            {
+                background.color = Color.clear;
+            }
+        }
+    }
+
+    void SetText()
+    {
+        txt.text = dialogue[curDia];
+        if (Input.GetButtonDown("Attack") == true && background.color == Color.white && nextDiaIndicator.color == textColor)
+        {
+            txt.color = Color.clear;
+        }
+        if (txt.color != textColor)
+        {
+            txt.color = Color.Lerp(txt.color, textColor, transitionTime);
+            nextDiaIndicator.color = Color.clear;
+        }
+        else if (nextDiaIndicator.color != textColor)
+        {
+            nextDiaIndicator.color = Color.Lerp(nextDiaIndicator.color, textColor, transitionTime);
+        }
+    }
+}
