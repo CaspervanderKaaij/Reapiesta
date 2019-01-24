cc738880bbde6cfb35abd045a0b6bcf0de1ac66d
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2019-01-24 10:28:40 +0100

    Update RNGMaterialColor.cs

--- Reapiesta/Assets/RNGMaterialColor.cs
+++ Reapiesta/Assets/RNGMaterialColor.cs
@@ -4,67 +4,26 @@ using UnityEngine;
 
 public class RNGMaterialColor : MonoBehaviour
 {
-	[SerializeField] Color[] colorBank;
+    [SerializeField] Color[] colorBank;
+    [SerializeField] Color[] colorBank1;
+    [SerializeField] Color test1;
+	[SerializeField] Color test2;
+
     void Start()
     {
-		int index = Random.Range(0, colorBank.Length);
-		Renderer r = transform.GetChild(0).GetComponent<Renderer>();
-		r.material.color = colorBank[index];
-		#region  Test
-        Color rngColor = Color.red;
-        int rng = 0;
-        rng = Random.Range(0, 7);
-        switch (rng)
-        {
-            case 0:
-                rngColor = Color.red;
-                break;
-            case 1:
-                rngColor = Color.blue;
-                break;
-            case 2:
-                rngColor = Color.green;
-                break;
-            case 3:
-                rngColor = Color.yellow;
-                break;
-            case 4:
-                rngColor = Color.cyan;
-                break;
-            case 5:
-                rngColor = Color.magenta;
-                break;
-            case 6:
-                rngColor = Color.grey;
-                break;
-            case 7:
-                rngColor = Color.white;
-                break;
-        }
-        transform.GetChild(0).GetComponent<Renderer>().material.color = rngColor;
-        rng = Random.Range(0, 5);
-        switch (rng)
-        {
-            case 0:
-                rngColor = Color.red;
-                break;
-            case 1:
-                rngColor = Color.blue;
-                break;
-            case 2:
-                rngColor = Color.green;
-                break;
-            case 3:
-                rngColor = Color.yellow;
-                break;
-            case 4:
-                rngColor = Color.cyan;
-                break;
-            case 5:
-                rngColor = Color.magenta;
-                break;
-        }
-        transform.GetChild(1).GetComponent<Renderer>().material.color = rngColor;
-		#endregion
+        int index0 = Random.Range(0, colorBank.Length);
+        Renderer r0 = transform.GetChild(0).GetComponent<Renderer>();
+        r0.material.color = colorBank[index0];
+
+        int index1 = Random.Range(0, colorBank1.Length);
+        Renderer r1 = transform.GetChild(1).GetComponent<Renderer>();
+        r1.material.color = colorBank1[index1];
+		test1 = colorBank[index0];
+		test2 = colorBank1[index1];       
+		/* 
+        int index2 = Random.Range(0, colorBank.Length);
+        Renderer r2 = transform.GetChild(0).GetComponent<Renderer>();
+        r0.material.color = colorBank[index2];
+		*/
     }
 }
